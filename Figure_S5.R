e6029d427321c2c1d4ea5ebd1f938b844303e0ce
Charlotte Francoeur <32343009+cfrancoeur@users.noreply.github.com>
2019-12-05 19:04:07 -0600

    Update and rename Figure_S6.R to Figure_S5.R

--- Figure_S5.R
+++ Figure_S5.R
@@ -9,7 +9,7 @@ library(ggpubr)
 library(DECIPHER)
 library(phangorn)
 
-#Figure S6A, based on code from https://benjjneb.github.io/dada2/tutorial.html
+#Figure S5A, based on code from https://benjjneb.github.io/dada2/tutorial.html
 
 top50 <- names(sort(taxa_sums(ps), decreasing=TRUE))[1:50]
 ps.top50 <- transform_sample_counts(ps, function(OTU) OTU/sum(OTU))
@@ -34,7 +34,7 @@ ggplot(pd0, aes(x = compound_dose, y = Abundance, fill = Phylum)) +
   geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + facet_wrap(~colony, scales="free_x", labeller=labeller(colony=colony.labels)) + scale_x_discrete(limits=c("Control5", "Control25", "ap5","ap25", "lina5", "lina25","noants"))
 
 
-##Figure S6B, based on code from https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html###
+##Figure S5B, based on code from https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html###
 plot_abundance = function(physeq,title = "",
                           Facet = "Order", Color = "Genus"){
   p1f = subset_taxa(physeq, Genus %in% c("Pantoea", "Klebsiella", "Pseudomonas","Serratia", "Burkholderia-Caballeronia-Paraburkholderia", "Enterobacter","Erwinia","Acinetobacter","Weissella"))
